2ee0183520648fba5970773764772e771dccadcf
all4sops2013-ui <all4sops2013@gmail.com>
2026-06-06 13:47:20 +0100

    Add files via upload

--- for loops2.py
+++ for loops2.py
@@ -4,3 +4,22 @@ num = int(input("Enter a number: "))
 for i in range(1,9,1):
     num = num - 1
     print(num)
+
+
+
+i=1
+while i<=14:
+    print(i)
+    i+=1
+print('Done !!')
+
+
+
+# WAP to take a number from the user and print the number of digits in that number using a while loop
+
+num = int(input("Enter a number: "))
+count = 0
+while num!= 0:
+    num=num//10
+    count=count+1
+print(count)
